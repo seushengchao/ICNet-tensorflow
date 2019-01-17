73c06b074c4a78370df6e562048ef107c03746e6
shengchao <isshengchao@yeah.net>
2019-01-17 17:09:57 +0800

    Revert "Merge branch 'ubuntutest'"
    This reverts commit daff468c6472d3f8e0e03d8813311cca9033f437.

--- train_1.py
+++ train_1.py
@@ -17,31 +17,4 @@ a = b + Config
 
 shengchao = 5
 jiangkai = 6
-<<<<<<< HEAD
 cnacan = 7
-=======
-cnacan = 7
-
-def get_arguments():
-    parser = argparse.ArgumentParser(description="Reproduced ICNet")
-    
-    parser.add_argument("--random-mirror", action="store_true",
-                        help="Whether to randomly mirror the inputs during the training.")
-    parser.add_argument("--random-scale", action="store_true",
-                        help="Whether to randomly scale the inputs during the training.")
-    parser.add_argument("--update-mean-var", action="store_true",
-                        help="whether to get update_op from tf.Graphic_Keys")
-    parser.add_argument("--train-beta-gamma", action="store_true",
-                        help="whether to train beta & gamma in bn layer")
-    parser.add_argument("--dataset", required=True,
-                        help="Which dataset to trained with",
-                        choices=['cityscapes', 'ade20k', 'others'])
-    parser.add_argument("--filter-scale", type=int, default=1,
-                        help="1 for using pruned model, while 2 for using non-pruned model.",
-                        choices=[1, 2])
-    return parser.parse_args()
-
-
-shengchao = 1
-
->>>>>>> ubuntutest
